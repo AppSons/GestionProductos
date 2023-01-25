e26456ce3524ab29e1660d634b9afb2f3cf29f35
Fernando Nielson <appsons.business@gmail.com>
2023-01-25 17:19:31 +0100

    Add Productos component

--- src/app/components/dashboard/productos/productos.component.ts
+++ src/app/components/dashboard/productos/productos.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA, MatDialog} from '@angular/material/dialog';
+import { Producto } from 'src/app/interfaces/productos';
+import { ProductosService } from 'src/app/services/productos.service';
+
 
 @Component({
   selector: 'app-productos',
@@ -7,9 +12,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductosComponent implements OnInit {
 
-  constructor() { }
+  formato: any[] = ['Envasado', 'Kilo', 'Litro', 'Unidad'];
+  form: FormGroup;
+    
+  
 
-  ngOnInit(): void {
+  constructor(private _fb: FormBuilder, private _productservice: ProductosService, public dialog: MatDialog,
+             public dialogRefProd: MatDialogRef<ProductosComponent>,
+              @Inject(MAT_DIALOG_DATA) public producto: Producto) {
+
+    this.form = this._fb.group({
+      nombre: ['', Validators.required],
+      precio: ['', Validators.required],
+      formato: ['', Validators.required],
+      marca: ['', Validators.required],
+    });
+    }
+
+  ngOnInit() {
   }
 
+  cancelar() {
+    this.dialogRefProd.close();
+  }
+
+  
+  agregarP() {   
+    const producto: Producto = {
+      nombre: this.form.value.nombre,
+      precio: this.form.value.precio,
+      formato: this.form.value.formato,
+      marca: this.form.value.marca,
+    }
+    this._productservice.agregarProducto(producto);
+    this.dialogRefProd.close();
+    
+    
+  }
+  
+  
+
+
+  
 }
