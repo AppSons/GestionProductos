e31963fa43ecbfe3945d44b2cdf4621f540da9eb
Fernando Nielson <appsons.business@gmail.com>
2023-01-25 17:32:24 +0100

    Add Drag&Drop Rows-beta

--- src/app/components/dashboard/draganddrop-tabla-dialog/draganddrop-tabla-dialog.component.ts
+++ src/app/components/dashboard/draganddrop-tabla-dialog/draganddrop-tabla-dialog.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+
+import {  CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+
+
+import { MatDialogRef, MAT_DIALOG_DATA, MatDialog} from '@angular/material/dialog';
+import { Columnas } from 'src/app/interfaces/columnas';
 
 @Component({
   selector: 'app-draganddrop-tabla-dialog',
@@ -6,10 +12,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./draganddrop-tabla-dialog.component.css']
 })
 export class DraganddropTablaDialogComponent implements OnInit {
+  columnas = [
+    'Nombre',
+    'Precio',
+    'Formato',
+    'Marca'
+  ]
+    
+  drop(event: Event)  {
+    if (this.isDragDrop(event)) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    }
+  }
 
-  constructor() { }
+  isDragDrop(object: any): object is CdkDragDrop<string[]> {
+    return 'previousIndex' in object;
+  }
+
+
+ 
+  
+  constructor(public dialog: MatDialog,
+              public dialogRefCol: MatDialogRef<DraganddropTablaDialogComponent>,
+              @Inject(MAT_DIALOG_DATA) _columnas: Columnas ) { }
 
   ngOnInit(): void {
   }
+
+  
+  cancelar() {
+    this.dialogRefCol.close();
+  }
+
+  
 
 }
