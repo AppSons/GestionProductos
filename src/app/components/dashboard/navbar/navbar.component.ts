64bd473dba11e66da8b6e7c6922e20c46159adce
Fernando Nielson <appsons.business@gmail.com>
2023-01-25 17:20:53 +0100

    Add Navbar Component

--- src/app/components/dashboard/navbar/navbar.component.ts
+++ src/app/components/dashboard/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+ 
+  userConnected!: string | null;
+  connected = false ;
 
-  constructor() { }
+  
+  constructor(private router: Router, private userService: UserService) { }
 
   ngOnInit(): void {
+    this.userConnected = this.userService.obtener_localStorage();
+    if(this.userConnected === null){
+      this.connected = false;
+      console.log(this.connected);
+      console.log(this.userConnected)
+    }else{
+      this.connected = true;
+      console.log(this.connected);
+      console.log(this.userConnected)
+    };
   }
 
+  clearStorage() {
+    localStorage.clear();
+    this.connected = false;
+    console.log(this.connected);
+    this.router.navigate(['/inicio']);
+  }
 }
